876aeb785f2dbc58e9e54eaa07c412db4b8ad0cb
Aaro <aaro.h4malainen@gmail.com>
2024-07-02 14:58:58 +0300

    Changes to code for the start

--- srcs/main.c
+++ srcs/main.c
@@ -1,39 +1,53 @@
 #include "../includes/cub3d.h"
 
-void	save_map(t_map *map)
+#include <stdio.h>
+
+void	save_map(t_map *map, char *map_name)
 {
-	map->mapsave = malloc((map->linecount + 1) * sizeof(char *));
-    *map->mapsave = get_next_line(map->fd);
-	while (*map->mapsave != NULL)
+	char	**arr;
+	int		fd;
+	int		count;
+
+	count = 0;
+	fd = open(map_name, O_RDONLY);
+	arr = malloc((map->linecount + 1) * sizeof(char *));
+	while (count != map->linecount)
 	{
-		map->mapsave++;
-		*map->mapsave = get_next_line(map->fd);
+		arr[count] = get_next_line(map->fd);
+		printf(arr[count]);
+		count++;
 	}
+	close(fd);
 }
 
-void read_map(t_map *map)
+void read_map(t_map *map, char *map_name)
 {
 	char	*line;
 	int		count;
+	int		fd;
 
-    line = get_next_line(map->fd);
+	count = 0;
+	fd = open(map_name, O_RDONLY);
+    line = get_next_line(fd);
 	while (line != NULL)
 	{
 		free(line);
-		line = get_next_line(map->fd);
+		line = get_next_line(fd);
 		count++;
 	}
+	close(fd);
 	map->linecount = count;
-	save_map(map);
+	save_map(map, map_name);
 }
 
 int main(int argc, char **argv)
 {
 	int	fd;
+	char	*map_name;
 	t_map map;
 
-	ft_memset(&map, 0, sizeof(map));
-	map.fd = open(argv[1], O_RDONLY);
-	read_map(&map);
+	map_name = argv[1];
+//	ft_memset(&map, 0, sizeof(map));
+	read_map(&map, map_name);
 	return (0);
 }
